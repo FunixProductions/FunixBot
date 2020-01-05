a5f3e738145cb07b43dd63bc2235e86054a96d01
Antoine PRONNIER <antoine.pronnier@epitech.eu>
2020-01-05 21:36:30 +0100

    changed giveaway system -> now register giveaway data and participants in .json files

--- commands/giveaway.js
+++ commands/giveaway.js
@@ -1,10 +1,10 @@
 const Twitch = require('../modules/twitch');
 const Logs = require('../modules/logs');
+const fs = require('fs');
 
-let giveaway = false;
-let subOnly = false;
-let word = '';
-let userList = [];
+const dirDataPath = './data/';
+const usersGiveawayPath = dirDataPath + 'giveawayUsers.json';
+const configGiveawayPath = dirDataPath + 'giveawayConfig.json';
 
 class Giveaway {
     static init(client, user, args, target) {
@@ -14,8 +14,10 @@ class Giveaway {
                 return;
             }
             let status = args[0].toLowerCase();
+            checkGiveawayFile();
+            let configGiveaway = JSON.parse(fs.readFileSync(configGiveawayPath));
             if (status === 'start') {
-                if (giveaway === true) {
+                if (configGiveaway.giveaway === true) {
                     client.say(target, "Un giveaway est déjà en cours. (!giveaway stop)");
                     return;
                 }
@@ -23,41 +25,46 @@ class Giveaway {
                     client.whisper(user['display-name'], "Giveaway : Commande incorrecte -> !giveaway [start|roll|stop] [mot gagnant] (sub) l'option sub n'est pas obligatoire.");
                     return;
                 }
+                emptyGiveawayFile();
                 let wordWin = args[1];
-                userList = [];
-                subOnly = false;
-                word = wordWin.toLowerCase();
-                giveaway = true;
+                configGiveaway.subOnly = false;
+                configGiveaway.word = wordWin.toLowerCase();
+                configGiveaway.giveaway = true;
                 if (args.length >= 3 && args[2].toLowerCase() === 'sub') {
-                    subOnly = true;
+                    configGiveaway.subOnly = true;
                 }
-                client.say(target, "HolidayPresent Giveaway lancé ! Mot clé -> " + word + (subOnly ? " (réservé aux abonnés sub twitch)" : ''));
-                console.log("[Giveaway] Lancé par : " + user['display-name'] + ". Mot clé -> " + word + (subOnly ? " pour les subs." : ''));
-                Logs.logSystem("[Giveaway] Lancé par : " + user['display-name'] + ". Mot clé -> " + word + (subOnly ? " pour les subs." : ''));
+                client.say(target, "HolidayPresent Giveaway lancé ! Mot clé -> " + configGiveaway.word + (configGiveaway.subOnly ? " (réservé aux abonnés sub twitch)" : ''));
+                console.log("[Giveaway] Lancé par : " + user['display-name'] + ". Mot clé -> " + configGiveaway.word + (configGiveaway.subOnly ? " pour les subs." : ''));
+                Logs.logSystem("[Giveaway] Lancé par : " + user['display-name'] + ". Mot clé -> " + configGiveaway.word + (configGiveaway.subOnly ? " pour les subs." : ''));
+                fs.writeFileSync(configGiveawayPath, JSON.stringify(configGiveaway));
             } else if (status === 'roll') {
-                if (giveaway === false) {
+                if (configGiveaway.giveaway === false) {
                     client.say(target, "Aucun giveaway en cours.");
                     return;
                 }
-                let winner = getWinner();
+                let userList = getParticipants();
+                let winner = getWinner(userList);
                 if (winner === null) {
                     client.say(target, "HolidayPresent Aucun participant pour le giveaway. Fin du concours.");
                     Logs.logSystem("[Giveaway] Aucun participant pour le giveaway. Fin du concours.");
-                    giveaway = false;
+                    configGiveaway.giveaway = false;
+                    emptyGiveawayFile();
                 } else {
                     client.say(target, "HolidayPresent Le gagnant est : " + winner);
-                    client.whisper(user['display-name'], "Gagnant du concours avec le mot : " + word + " est -> " + winner);
-                    console.log("[Giveaway] Giveaway" + (subOnly ? " sub" : '') + " [" + word + "], gagnant : " + winner);
-                    Logs.logSystem("[Giveaway] Giveaway" + (subOnly ? " sub" : '') + " [" + word + "], gagnant : " + winner);
+                    client.whisper(user['display-name'], "Gagnant du concours avec le mot : " + configGiveaway.word + " est -> " + winner);
+                    console.log("[Giveaway] Giveaway" + (configGiveaway.subOnly ? " sub" : '') + " [" + configGiveaway.word + "], gagnant : " + winner);
+                    Logs.logSystem("[Giveaway] Giveaway" + (configGiveaway.subOnly ? " sub" : '') + " [" + configGiveaway.word + "], gagnant : " + winner);
                     removeWinner(winner);
                 }
+                fs.writeFileSync(configGiveawayPath, JSON.stringify(configGiveaway));
             } else if (status === 'stop') {
-                if (giveaway === false) return;
-                giveaway = false;
-                userList = [];
+                if (configGiveaway.giveaway === false) return;
+                configGiveaway.giveaway = false;
+                emptyGiveawayFile();
                 client.say(target, "HolidayPresent Giveaway terminé !");
                 console.log("[Giveaway] - Giveaway terminé");
                 Logs.logSystem("[Giveaway] - Giveaway terminé");
+                fs.writeFileSync(configGiveawayPath, JSON.stringify(configGiveaway));
             } else {
                 client.whisper(user['display-name'], "Giveaway : Commande incorrecte -> !giveaway [start|roll|stop] [mot gagnant] (sub) l'option sub n'est pas obligatoire.");
             }
@@ -65,17 +72,21 @@ class Giveaway {
     }
 
     static participant(client, user, message) {
-        if (giveaway === true) {
+        checkGiveawayFile();
+        let configGiveaway = JSON.parse(fs.readFileSync(configGiveawayPath));
+        if (configGiveaway.giveaway === true) {
             message = message.toLowerCase();
-            if (userInList(user['display-name'])) {
+            if (isParticipant(user['display-name'])) {
                 return;
             }
-            if (message === word) {
-                if (subOnly && !Twitch.isSub(user)) {
+            if (message === configGiveaway.word) {
+                if (configGiveaway.subOnly && !Twitch.isSub(user)) {
                     client.whisper(user['display-name'], "Le giveaway est réservé aux abonnés (sub) twitch");
                     return;
                 }
-                userList.push(user['display-name']);
+                if (!isParticipant(user['display-name'])) {
+                    addNewuser(user['display-name']);
+                }
                 console.log("[Giveaway] - Ajout de " + user['display-name'] + " à la liste");
                 client.whisper(user['display-name'], "Participation au giveaway prise en compte !");
             }
@@ -83,23 +94,62 @@ class Giveaway {
     }
 }
 
-function removeWinner(user) {
-    let index = userList.indexOf(user);
-    if (index > -1) {
-        userList.splice(index, 1);
+function emptyGiveawayFile() {
+    checkGiveawayFile();
+    fs.writeFileSync(usersGiveawayPath, JSON.stringify([]));
+}
+
+function checkGiveawayFile() {
+    if (!fs.existsSync(dirDataPath)) {
+        fs.mkdirSync(dirDataPath);
+    }
+    if (!fs.existsSync(usersGiveawayPath)) {
+        fs.appendFileSync(usersGiveawayPath, []);
+    }
+    if (!fs.existsSync(configGiveawayPath)) {
+        fs.appendFileSync(configGiveawayPath, JSON.stringify({
+            'giveaway': false,
+            'subOnly': false,
+            'word': ''
+        }))
     }
 }
 
-function userInList(user) {
-    for (let i = 0; i < userList.length; ++i) {
-        if (userList[i] === user) {
+function isParticipant(userName) {
+    checkGiveawayFile();
+    let users = JSON.parse(fs.readFileSync(usersGiveawayPath));
+    for (let i = 0; i < users.length; ++i) {
+        if (users[i] === userName) {
             return true;
         }
     }
     return false;
 }
 
-function getWinner() {
+function addNewuser(userName) {
+    checkGiveawayFile();
+    let users = JSON.parse(fs.readFileSync(usersGiveawayPath));
+    users = JSON.stringify(users.concat([userName]));
+    fs.writeFileSync(usersGiveawayPath, users);
+}
+
+function getParticipants() {
+    checkGiveawayFile();
+    return JSON.parse(fs.readFileSync(usersGiveawayPath));
+}
+
+function removeWinner(user) {
+    checkGiveawayFile();
+    let userList = JSON.parse(fs.readFileSync(usersGiveawayPath));
+    let index = userList.indexOf(user);
+    if (index > -1) {
+        userList.splice(index, 1);
+    }
+    userList = JSON.stringify(userList);
+    fs.writeFileSync(usersGiveawayPath, userList);
+}
+
+function getWinner(userList) {
     const nbrUsers = userList.length;
     if (nbrUsers === 0) {
         return null;
