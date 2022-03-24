62dd7f6a8290ed7997231b0e91c58fcbb37d729b
Antoine PRONNIER (Funix) <funixgaming7@gmail.com>
2022-03-24 16:16:07 +0200

    base impl of commands

---
+++ Core/src/test/java/fr/funixgaming/funixbot/core/commands/CommandHandlerTests.java
@@ -0,0 +1,39 @@
+package fr.funixgaming.funixbot.core.commands;
+
+import fr.funixgaming.twitch.api.chatbot_irc.entities.ChatMember;
+import fr.funixgaming.twitch.api.chatbot_irc.parsers.TagParser;
+import org.junit.jupiter.api.Test;
+
+import java.time.Instant;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class CommandHandlerTests {
+
+    @Test
+    public void testChatCommand() {
+        final String commandTest = "test";
+        final String message = "!test salut les potes";
+        AtomicBoolean eventTriggered = new AtomicBoolean(false);
+
+        final UserCommandEvent commandEvent = (user, command, args) -> {
+            assertEquals(commandTest, command);
+            assertEquals(3, args.length);
+            assertEquals("salut", args[0]);
+            assertEquals("les", args[1]);
+            assertEquals("potes", args[2]);
+            eventTriggered.set(true);
+        };
+
+        final CommandHandler commandHandler = CommandHandler.getInstance();
+        commandHandler.addListener(commandTest, commandEvent);
+        commandHandler.onNewChat(null, message);
+
+        final Instant limit = Instant.now().plusSeconds(10);
+        while (!eventTriggered.get() && Instant.now().isBefore(limit));
+        assertTrue(eventTriggered.get());
+    }
+
+}
--- FunixBot-Twitch/src/main/java/fr/funixgaming/funixbot/twitch/events/FunixBotEvents.java
+++ FunixBot-Twitch/src/main/java/fr/funixgaming/funixbot/twitch/events/FunixBotEvents.java
@@ -1,22 +1,28 @@
 package fr.funixgaming.funixbot.twitch.events;
 
+import fr.funixgaming.funixbot.core.commands.CommandHandler;
 import fr.funixgaming.funixbot.twitch.FunixBot;
 import fr.funixgaming.twitch.api.chatbot_irc.TwitchEvents;
+import fr.funixgaming.twitch.api.chatbot_irc.entities.ChatMember;
 import fr.funixgaming.twitch.api.chatbot_irc.entities.ChatMessage;
 import fr.funixgaming.twitch.api.chatbot_irc.events.UserChatEvent;
 
 public class FunixBotEvents implements TwitchEvents {
 
     private final FunixBot bot;
+    private final CommandHandler commandHandler;
 
     public FunixBotEvents(final FunixBot bot) {
         this.bot = bot;
+        this.commandHandler = CommandHandler.getInstance();
     }
 
     @Override
     public void onUserChat(UserChatEvent event) {
         final ChatMessage chatMessage = event.getChatMessage();
+        final ChatMember chatMember = event.getChatMember();
+        final String message = chatMessage.getMessage();
 
-        bot.sendMessageToChannel("funixgaming", "COUCOU " + chatMessage.getOwner().getDisplayName());
+        commandHandler.onNewChat(chatMember, message);
     }
 }
