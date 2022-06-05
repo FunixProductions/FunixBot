77e29a021094f191a53d5690091400a62762b2bc
Antoine PRONNIER (Funix) <antoine.pronnier@gmail.com>
2022-06-05 19:27:14 +0300

    command giveaway

--- FunixBot-Twitch/src/main/java/fr/funixgaming/funixbot/twitch/utils/TwitchEmotes.java
+++ FunixBot-Twitch/src/main/java/fr/funixgaming/funixbot/twitch/utils/TwitchEmotes.java
@@ -2,4 +2,5 @@ package fr.funixgaming.funixbot.twitch.utils;
 
 public class TwitchEmotes {
     public final static String TWITCH_LOGO = "imGlitch";
+    public final static String GIFT = "HolidayPresent";
 }
