3a86d3f8993467500d61ea692b682e58eb9d9063
Antoine PRONNIER (Funix) <antoine.pronnier@gmail.com>
2022-06-05 18:17:16 +0300

    improved exp

--- FunixBot-Twitch/src/main/java/fr/funixgaming/funixbot/twitch/commands/LevelCommand.java
+++ FunixBot-Twitch/src/main/java/fr/funixgaming/funixbot/twitch/commands/LevelCommand.java
@@ -1,45 +1,45 @@
 package fr.funixgaming.funixbot.twitch.commands;
 
-import fr.funixgaming.api.client.funixbot.clients.FunixBotUserExperienceClient;
 import fr.funixgaming.api.client.funixbot.dtos.FunixBotUserExperienceDTO;
 import fr.funixgaming.funixbot.core.commands.entities.BotCommand;
+import fr.funixgaming.funixbot.core.exceptions.FunixBotException;
 import fr.funixgaming.funixbot.twitch.FunixBot;
+import fr.funixgaming.funixbot.twitch.modules.ChatExperience;
 import fr.funixgaming.funixbot.twitch.utils.TwitchEmotes;
 import fr.funixgaming.twitch.api.chatbot_irc.entities.ChatMember;
 import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.List;
-
+@Slf4j
 public class LevelCommand extends BotCommand {
     private final FunixBot funixBot;
-
-    private final FunixBotUserExperienceClient userExperienceClient;
 
     public LevelCommand(final FunixBot bot) {
         super("level", "lvl", "lv");
         this.funixBot = bot;
-
-        this.userExperienceClient = bot.getFunixBotUserExperienceClient();
     }
 
     @Override
     public void onUserCommand(@NonNull ChatMember user, @NonNull String command, @NotNull @NonNull String[] args) {
-        final String channel = user.getChannelName();
-        final List<FunixBotUserExperienceDTO> experience = userExperienceClient.search(String.format("twitchUserId:%d", user.getUserId()), "0", "1");
+        try {
+            final String channel = user.getChannelName();
+            final FunixBotUserExperienceDTO userExperience = ChatExperience.getInstance().findExpByUserId(Integer.toString(user.getUserId()));
 
-        if (experience.isEmpty()) {
-            funixBot.sendChatMessage(channel, String.format("%s n'a pas encore de niveau sur ce stream.", user.getDisplayName()));
-        } else {
-            final FunixBotUserExperienceDTO userExperience = experience.get(0);
-            funixBot.sendChatMessage(channel, String.format(
-                    "%s %s niveau %d (%d/%d)",
-                    TwitchEmotes.TWITCH_LOGO,
-                    user.getDisplayName(),
-                    userExperience.getLevel(),
-                    userExperience.getXp(),
-                    userExperience.getXpNextLevel())
-            );
+            if (userExperience == null) {
+                funixBot.sendChatMessage(channel, String.format("%s n'a pas encore de niveau sur ce stream.", user.getDisplayName()));
+            } else {
+                funixBot.sendChatMessage(channel, String.format(
+                        "%s %s niveau %d (%d/%d)",
+                        TwitchEmotes.TWITCH_LOGO,
+                        user.getDisplayName(),
+                        userExperience.getLevel(),
+                        userExperience.getXp(),
+                        userExperience.getXpNextLevel())
+                );
+            }
+        } catch (FunixBotException e) {
+            log.error("Erreur !level command: {}", e.getMessage());
         }
     }
 }
--- FunixBot-Twitch/src/main/java/fr/funixgaming/funixbot/twitch/events/FunixBotEvents.java
+++ FunixBot-Twitch/src/main/java/fr/funixgaming/funixbot/twitch/events/FunixBotEvents.java
@@ -4,7 +4,7 @@ import fr.funixgaming.funixbot.core.commands.CommandHandler;
 import fr.funixgaming.funixbot.core.exceptions.FunixBotException;
 import fr.funixgaming.funixbot.twitch.FunixBot;
 import fr.funixgaming.funixbot.twitch.modules.AutoMessages;
-import fr.funixgaming.funixbot.twitch.modules.ChatExpGain;
+import fr.funixgaming.funixbot.twitch.modules.ChatExperience;
 import fr.funixgaming.twitch.api.chatbot_irc.TwitchEvents;
 import fr.funixgaming.twitch.api.chatbot_irc.entities.ChatMember;
 import fr.funixgaming.twitch.api.chatbot_irc.entities.ChatMessage;
@@ -20,7 +20,7 @@ public class FunixBotEvents implements TwitchEvents {
 
     private final CommandHandler commandHandler;
     private final AutoMessages autoMessages;
-    private final ChatExpGain chatExpGain;
+    private final ChatExperience chatExperience;
 
     @Override
     public void onUserChat(UserChatEvent event) {
@@ -31,11 +31,11 @@ public class FunixBotEvents implements TwitchEvents {
         try {
             commandHandler.onNewChat(chatMember, message, FunixBot.getInstance(), chatMember.getChannelName());
             autoMessages.userMessage();
-            chatExpGain.userChatExp(chatMember);
+            chatExperience.userChatExp(chatMember);
 
             log.info("[" + chatMember.getDisplayName() + "] " + message);
         } catch (FunixBotException e) {
-            e.printStackTrace();
+            log.error("Erreur chat event: {}", e.getMessage());
         }
     }
 }
