825041073b7e49771c78764337b366dedeed76bf
Antoine PRONNIER (Funix) <antoine.pronnier@gmail.com>
2022-06-07 03:12:48 +0300

    wip discord bot

---
+++ FunixBot-Discord/src/main/java/fr/funixgaming/funixbot/discord/FunixBot.java
@@ -0,0 +1,33 @@
+package fr.funixgaming.funixbot.discord;
+
+import fr.funixgaming.funixbot.core.Bot;
+import fr.funixgaming.funixbot.core.commands.entities.BotCommand;
+
+import java.util.Set;
+
+public class FunixBot implements Bot {
+    @Override
+    public void sendChatMessage(String channel, String message) {
+
+    }
+
+    @Override
+    public void addNewCommand(BotCommand command) {
+
+    }
+
+    @Override
+    public void removeCommand(String commandName) {
+
+    }
+
+    @Override
+    public void stopBot() {
+
+    }
+
+    @Override
+    public Set<BotCommand> getCommands() {
+        return null;
+    }
+}
