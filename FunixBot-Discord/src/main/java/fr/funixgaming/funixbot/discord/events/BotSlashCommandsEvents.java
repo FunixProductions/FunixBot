e2fe4bf04d71cd05e91004e45730d457ea3130b6
iGameCreep <110474721+iGameCreep@users.noreply.github.com>
2023-02-24 13:52:08 +0100

    Update BotSlashCommandsEvents.java

--- FunixBot-Discord/src/main/java/fr/funixgaming/funixbot/discord/events/BotSlashCommandsEvents.java
+++ FunixBot-Discord/src/main/java/fr/funixgaming/funixbot/discord/events/BotSlashCommandsEvents.java
@@ -1,10 +1,12 @@
 package fr.funixgaming.funixbot.discord.events;
 
 import fr.funixgaming.funixbot.discord.FunixBot;
+import fr.funixgaming.funixbot.discord.commands.*;
 import lombok.RequiredArgsConstructor;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
+
 
 @RequiredArgsConstructor
 public class BotSlashCommandsEvents extends ListenerAdapter {
@@ -12,6 +14,25 @@ public class BotSlashCommandsEvents extends ListenerAdapter {
     private final FunixBot funixBot;
 
     @Override
-    public void onSlashCommandInteraction(@NotNull SlashCommandInteractionEvent event) {
+    public void onSlashCommandInteraction(SlashCommandInteractionEvent event) {
+        super.onSlashCommandInteraction(event);
+
+        switch (event.getName()) {
+            case "gnou" :
+                Commands.gnou.run(event);
+                break;
+            case "ip":
+                EmbedCommands.ip.run(event);
+                break;
+            case "ping":
+                EmbedCommands.ping.run(event);
+                break;
+            case "me":
+                EmbedCommands.me.run(event);
+                break;
+            case "help":
+                EmbedCommands.help.run(event);
+        }
+
     }
 }
