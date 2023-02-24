e64fc443267320cf6460cf13c2103fc49545b9c4
iGameCreep <110474721+iGameCreep@users.noreply.github.com>
2023-02-24 13:18:11 +0100

    Update BotMessagesEvents.java

--- FunixBot-Discord/src/main/java/fr/funixgaming/funixbot/discord/events/BotMessagesEvents.java
+++ FunixBot-Discord/src/main/java/fr/funixgaming/funixbot/discord/events/BotMessagesEvents.java
@@ -39,18 +39,72 @@ public class BotMessagesEvents extends ListenerAdapter {
     }
 
     @Override
-    public void onMessageReactionAdd(@NotNull MessageReactionAddEvent event) {
-        final User user = event.getUser();
-        final MessageReaction reaction = event.getReaction();
+    public void onMessageReceived(@NotNull MessageReceivedEvent event) {
+        super.onMessageReceived(event);
+        final User user = event.getAuthor();
+        final Message message = event.getMessage();
+        final MessageChannel textChannel = event.getChannel();
 
-        roleMessageHandler.manageRolesByReactions(user, reaction, true);
+        if (event.getAuthor().isBot()) return;
+
+        log.info("[{} > {}] {}", user.getAsTag(), textChannel.getName(), message.getContentRaw());
+
+        if (event.getMessage().getContentDisplay().equals("!rolereact") && event.getAuthor().getId().equals("696753471650660412")){
+            EmbedBuilder embed = new EmbedBuilder()
+                    .setColor(new Color(44,175,255))
+                    .setTitle("**Choix des rôles**")
+                    .setDescription("Afin d'éviter de faire des tag everyone et here sur le discord, vous pouvez choisir vos rôles pour recevoir les notifications qui vous intéressent.\n" +
+                            "Vous pouvez d'ailleurs ajouter et retirer votre rôle à tout moment.")
+                    .addField("<:twitch:1076929000330498058>", "Les notifications Twitch", true)
+                    .addField("<:youtube:1076929043154346047>", "Les notifications YouTube", true)
+                    .addField("<:tiktok:1076929073516912733>", "Les notifications TikTok", true);
+            Button twitchbtn = Button.primary("notif-twitch", Emoji.fromCustom(":twitch:", Long.parseLong("1076929000330498058"), false));
+            Button youtubebtn = Button.primary("notif-youtube", Emoji.fromCustom(":youtube:", Long.parseLong("1076929043154346047"), false));
+            Button tiktokbtn = Button.primary("notif-tiktok", Emoji.fromCustom(":tiktok:", Long.parseLong("1076929073516912733"), false));
+
+            event.getChannel().sendMessageEmbeds(embed.build()).addActionRow(twitchbtn, youtubebtn, tiktokbtn).queue();
+            event.getMessage().delete().queue();
+        }
+
+        if (event.getMessage().getContentRaw().startsWith("!")) {
+            String command = event.getMessage().getContentRaw().split(" ")[0].replace("!", "");
+
+            for (CommandList cmdl : CommandList.values()) {
+                if (cmdl.getName().equals(command)) {
+                    for (EmbedCommands cmd : EmbedCommands.values()) {
+                        if (cmd.getName().equals(command)) {
+                            cmd.run(event, command);
+                        }
+                    }
+                    for (Commands cmd : Commands.values()) {
+                        if (cmd.getName().equals(command)) {
+                            cmd.run(event);
+                        }
+                    }
+                } else {
+                    errcommand(event, command);
+                }
+
+            }
+            setCount(0);
+        }
     }
 
-    @Override
-    public void onMessageReactionRemove(@NotNull MessageReactionRemoveEvent event) {
-        final User user = event.getUser();
-        final MessageReaction reaction = event.getReaction();
+    private int Count = 0;
 
-        roleMessageHandler.manageRolesByReactions(user, reaction, false);
+    public void errcommand(MessageReceivedEvent event, String command) {
+        setCount((getCount() + 1));
+        if (getCount() == CommandList.values().length) { // Command is invalid
+            event.getChannel().sendMessage(String.format(":warning: La commande `%s` n'existe pas ! \n`!help` pour la liste des commandes.", command)).queue();
+        }
     }
+
+    public int getCount() {
+        return Count;
+    }
+
+    public void setCount(int count) {
+        Count = count;
+    }
+    
 }
