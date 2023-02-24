641b8a03fa2695778a9989583545bfe1edd3d26a
iGameCreep <110474721+iGameCreep@users.noreply.github.com>
2023-02-24 14:16:58 +0100

    Update CommandList.java

--- FunixBot-Discord/src/main/java/fr/funixgaming/funixbot/discord/commands/CommandList.java
+++ FunixBot-Discord/src/main/java/fr/funixgaming/funixbot/discord/commands/CommandList.java
@@ -1,6 +1,6 @@
-package fr.gamecreep.bot.commands;
+package fr.funixgaming.funixbot.commands;
 
-import fr.gamecreep.bot.commands.*;
+package fr.funixgaming.funixbot.commands.*;
 
 public enum CommandList {
 
