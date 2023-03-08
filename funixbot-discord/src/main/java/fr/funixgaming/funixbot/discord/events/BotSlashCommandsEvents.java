0ec5e9587e0aab7d9f0295a8ec4ffcc8f0931930
iGameCreep <110474721+iGameCreep@users.noreply.github.com>
2023-03-08 14:14:31 +0100

    Update BotSlashCommandsEvents.java

--- funixbot-discord/src/main/java/fr/funixgaming/funixbot/discord/events/BotSlashCommandsEvents.java
+++ funixbot-discord/src/main/java/fr/funixgaming/funixbot/discord/events/BotSlashCommandsEvents.java
@@ -1,5 +1,7 @@
 package fr.funixgaming.funixbot.discord.events;
 
+import fr.funixgaming.funixbot.commands.utils.BotCommand;
+import fr.funixgaming.funixbot.commands.utils.CommandList;
 import fr.funixgaming.funixbot.discord.FunixBot;
 import lombok.RequiredArgsConstructor;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
@@ -12,6 +14,17 @@ public class BotSlashCommandsEvents extends ListenerAdapter {
     private final FunixBot funixBot;
 
     @Override
-    public void onSlashCommandInteraction(@NotNull SlashCommandInteractionEvent event) {
+    public void onSlashCommandInteraction(SlashCommandInteractionEvent event) {
+        super.onSlashCommandInteraction(event);
+
+        CommandList cmdList;
+        cmdList = new CommandList();
+        List<BotCommand> commandList = cmdList.getList();
+
+        for (BotCommand cmd : commandList) {
+            if (cmd.getName().equals(event.getName())) {
+                cmd.onUserCommand(event);
+            }
+        }
     }
 }
