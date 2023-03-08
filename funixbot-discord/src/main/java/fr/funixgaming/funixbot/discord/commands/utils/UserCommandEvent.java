5facb3775c3934211cb11dddcea562b6a2348c8e
iGameCreep <110474721+iGameCreep@users.noreply.github.com>
2023-03-08 14:12:07 +0100

    Update UserCommandEvent.java

--- funixbot-discord/src/main/java/fr/funixgaming/funixbot/discord/commands/utils/UserCommandEvent.java
+++ funixbot-discord/src/main/java/fr/funixgaming/funixbot/discord/commands/utils/UserCommandEvent.java
@@ -1,13 +1,9 @@
-package fr.gamecreep.bot.commands.utils;
+package fr.funixgaming.funixbot.commands.utils;
 
 import lombok.NonNull;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 
 public interface UserCommandEvent {
-
-    /*
-        Principalement repris du FunixBot Twitch.
-     */
 
     void onUserCommand(final @NonNull SlashCommandInteractionEvent interactionEvent);
 
