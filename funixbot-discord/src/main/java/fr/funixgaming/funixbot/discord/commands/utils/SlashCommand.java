52e025b639f4b477b8d00c148764559b24f85fab
GameCreep35 <110474721+iGameCreep@users.noreply.github.com>
2023-03-09 18:02:02 +0100

    Create SlashCommand.java

---
+++ funixbot-discord/src/main/java/fr/funixgaming/funixbot/discord/commands/utils/SlashCommand.java
@@ -0,0 +1,12 @@
+package fr.funixgaming.funixbot.commands.utils;
+
+import lombok.NonNull;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+
+public interface SlashCommand {
+
+    String getName();
+    String getDescription();
+    void runCommand(@NonNull SlashCommandInteractionEvent interactionEvent);
+
+}
